29526552ae0633f74f42e17f26571978fe9f0c66
henintsoaheriniaina <belouhtsoa@gmail.com>
2025-03-05 11:30:08 +0300

    fix: hero

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,10 +1,13 @@
 import { AlignJustify, Zap } from "lucide-react";
+import { useState } from "react";
 import { Button } from "./ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 
 const MobileNav = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <Sheet>
+    <Sheet open={open} onOpenChange={setOpen}>
       <SheetTrigger asChild>
         <Button
           variant="outline"
@@ -16,21 +19,33 @@ const MobileNav = () => {
       </SheetTrigger>
       <SheetContent side="left">
         <div className="p-4">
-          <a href="#" className="flex items-center gap-2 ">
+          <a href="#" className="flex items-center gap-2">
             <Zap />
             <p className="text-2xl font-bold">Henintsoa Heriniaina</p>
           </a>
           <div className="grid gap-2 mt-6">
-            <a href="#" className="mobile-link">
+            <a href="#" className="mobile-link" onClick={() => setOpen(false)}>
               Home
             </a>
-            <a href="#skills" className="mobile-link">
+            <a
+              href="#skills"
+              className="mobile-link"
+              onClick={() => setOpen(false)}
+            >
               Skills
             </a>
-            <a href="#projects" className="mobile-link">
+            <a
+              href="#projects"
+              className="mobile-link"
+              onClick={() => setOpen(false)}
+            >
               Projects
             </a>
-            <a href="#contact" className="mobile-link">
+            <a
+              href="#contact"
+              className="mobile-link"
+              onClick={() => setOpen(false)}
+            >
               Contact
             </a>
           </div>
